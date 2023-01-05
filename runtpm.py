d913b30ef8963edbcaae16edf095a6534df9bd69
rye-007 <sulaiman.weani@gmail.com>
2023-01-05 16:41:07 +0700

    5-1-66

--- runtpm.py
+++ runtpm.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('sideber.html')
+    return render_template('detaileqm2.html')
 
 if __name__ == "__main__":
     app.run(debug=True) 
