dec5da7b5e43e9f6b465dc33d855088964dac240
rye-007 <sulaiman.weani@gmail.com>
2023-01-05 13:51:12 +0700

    modified:   dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('license.html')
+    return render_template('dashboard.html')
 
 if __name__ == "__main__":
     app.run(debug=True) 
