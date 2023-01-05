a87dde97125f96e6bb90fd3ed95c77a3a17e3411
rye-007 <sulaiman.weani@gmail.com>
2023-01-05 15:00:15 +0700

    modified:   runtpm.py

--- runtpm.py
+++ runtpm.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('addeqm.html')
+    return render_template('sideber.html')
 
 if __name__ == "__main__":
     app.run(debug=True) 
